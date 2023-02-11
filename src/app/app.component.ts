56fc6ea2908305cfd768fd6fbc814d5db2396753
Ruben <skyline7514@gmail.com>
2023-02-11 11:35:01 -0700

    Funcionalidad basica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-project';
-
-  numero:number = 0;
-
-  sumarNumero(){
-    this.numero+= 1;
+  title = 'Node';
+  numero:number = 5;
+  numero2:number = 10;
+  cadena1:string = "hola ";
+  cadena2:string = "mundo";
+  cadena3:string = "";
+  resultado:number = 0;
+  sumar(){
+    this.resultado = this.numero + this.numero2;
   }
 
-  restarNumero(){
-    this.numero-= 1;
+  restar(){
+    this.resultado = this.numero - this.numero2;
+  }
+
+  dividir(){
+    if(this.numero === 0 || this.numero2 === 0){
+      console.log("Error");
+    }
+    else{
+      this.resultado = this.numero / this.numero2;
+    } 
+  }
+
+  multiplicar(){
+    this.resultado = this.numero * this.numero2;
+  }
+
+  sumarCadenas(){
+    this.cadena3 = this.cadena1 + this.cadena2;
+  }
+  restablecer(){
+    this.resultado = 0;
+    this.cadena3 = "";
   }
 }
