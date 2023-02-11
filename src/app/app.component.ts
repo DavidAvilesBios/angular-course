3bad4228e7b2b23948d4f0564e0baa49ec30cb46
4lexRz <ca.nolasco18@info.uas.edu.mx>
2023-02-11 13:01:09 -0700

    segundo if ejemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent {
   title = 'angular-project';
 
   numero:number = 0;
-
+  numero2:number = 0;
+  
   sumarNumero(){
     this.numero+= 1;
   }
@@ -17,6 +18,15 @@ export class AppComponent {
   restarNumero(){
     this.numero-= 1;
   }
+
+  sumarNum(){
+    this.numero2+= 1;
+  }
+
+  restarNum(){
+    this.numero2-= 1;
+  }
 }
+
 
 
